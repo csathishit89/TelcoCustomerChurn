4c906640cf05df0d992eb8a09e92ba04f52bfdc7
Sathish <sathish@kapiital.com>
2026-06-18 11:56:35 +0530

    updated for Dashboard view changes in EDA

--- app.py
+++ app.py
@@ -6,10 +6,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("C:\MAMP\htdocs\TelcoCustomerChurn\WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
 model = pickle.load(open('churn_model.pkl', 'rb'))
+
+# Convert TotalCharges
+df["TotalCharges"] = pd.to_numeric(
+    df["TotalCharges"],
+    errors='coerce'
+)
+
+df.fillna(0, inplace=True)
+
+# Encode categorical columns
+le = LabelEncoder()
+
+for col in df.select_dtypes(include='object').columns:
+    df[col] = le.fit_transform(df[col])
+
+# Load trained model
+model = pickle.load(open('churn_model.pkl', 'rb'))
+
+# Split
+X = df.drop("Churn", axis=1)
+y = df["Churn"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+# Ensure columns match model training columns
+X_test = X_test.reindex(
+    columns=model.feature_names_in_,
+    fill_value=0
+)
 
 st.set_page_config(
     page_title="Telco Customer Churn Prediction",
@@ -38,6 +73,22 @@ st.markdown("""
     div.stButton > button:first-child:hover {
         background-color: #D81B60;
         color: white;
+    }
+    
+    [data-testid="stSidebar"] {
+        min-width: 150px;
+        max-width: 150px;
+    }
+    
+    /* Collapsed sidebar width */
+    [data-testid="stSidebar"][aria-expanded="false"]{
+        min-width: 0px;
+        max-width: 0px;
+    }
+
+    /* Main content adjustment */
+    [data-testid="stSidebar"][aria-expanded="false"] + div {
+        margin-left: 0px;
     }
 </style>
 """, unsafe_allow_html=True)
@@ -167,6 +218,7 @@ if page == "Dashboard":
             data=df,
             ax=ax
         )
+        ax.set_xticklabels(['No', 'Yes'])
 
         st.pyplot(fig)
         
@@ -177,6 +229,8 @@ if page == "Dashboard":
         
         sns.countplot(x='gender', hue='Churn', data=df, ax=ax)
 
+        ax.set_xticklabels(['No', 'Yes'])
+        
         st.pyplot(fig)
         
     col1, col2 = st.columns(2)
@@ -192,7 +246,7 @@ if page == "Dashboard":
             data=df,
             ax=ax
         )
-
+        ax.set_xticklabels(['No', 'Yes'])
         st.pyplot(fig)
     
     with col2:
@@ -201,7 +255,7 @@ if page == "Dashboard":
         fig, ax = plt.subplots(figsize=(6,4))
 
         sns.histplot(df['MonthlyCharges'], kde=True)
-
+        
         st.pyplot(fig)
         
     col1, col2 = st.columns(2)
@@ -217,7 +271,7 @@ if page == "Dashboard":
             data=df,
             ax=ax
         )
-
+        ax.set_xticklabels(['No', 'Yes'])
         st.pyplot(fig)
         
     with col2:  
@@ -242,7 +296,7 @@ if page == "Dashboard":
             data=df,
             ax=ax
         )
-
+        ax.set_xticklabels(['Month-to-month','One year','Two year'])
         plt.xticks(rotation=20)
 
         st.pyplot(fig)
@@ -261,5 +315,53 @@ if page == "Dashboard":
             df_encoded[col] = le.fit_transform(df_encoded[col])
 
         sns.heatmap(df_encoded.corr(), cmap='coolwarm')
+        
+        st.pyplot(fig)
+        
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        
+        churn_ratio = df['Churn'].value_counts(normalize=True) * 100
+        fig, ax = plt.subplots(figsize=(8,4))
+    
+        plt.pie(
+            churn_ratio,
+            labels=churn_ratio.index,
+            autopct='%1.1f%%'
+        )
+
+        plt.title("Actual Churn Ratio")
+        plt.show()
+        
+        st.pyplot(fig)
+        
+    with col2:
+        # Probability of Churn = Yes
+        y_prob = model.predict_proba(X_test)[:,1]
+
+        # Sort for smooth sigmoid-like visualization
+        sorted_prob = np.sort(y_prob)
+
+        fig, ax = plt.subplots(figsize=(6,4))
+
+        plt.plot(
+            range(len(sorted_prob)),
+            sorted_prob
+        )
+
+        plt.axhline(
+            y=0.5,
+            linestyle='--',
+            label='Threshold = 0.5'
+        )
+
+        plt.xlabel("Customers (sorted)", fontsize=14)
+        plt.ylabel("Probability of Churn", fontsize=14)
+        plt.title("Predicted Probability for Telco Customer Churn", fontsize=16, pad=50)
+        plt.legend()
+        plt.grid()
+
+        plt.show()
         
         st.pyplot(fig)
